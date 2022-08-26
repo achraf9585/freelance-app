cf54270ebd838437bb8887b9f9e08e9c519859f8
achraf9585 <achraf9585@gmail.com>
2022-08-27 00:23:11 +0100

    'init'

---
+++ src/component/Footer.js
@@ -0,0 +1,91 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+<>
+<div class="section footer wf-section">
+      <div class="container">
+        <div class="footer-top-part">
+          <div id="w-node-_70b3894f-ad3a-f5e5-ddd7-7ba668131844-68131841">
+            <a href="index.html" data-w-id="70b3894f-ad3a-f5e5-ddd7-7ba668131845" aria-current="page" class="logo-link w-inline-block w--current"><img src="https://uploads-ssl.webflow.com/623b341060c39e4dc7b99525/623c6048d9f33b37ec6d4826_logo.png" loading="lazy" width="138" alt="" class="bo-logo"/>
+              <p class="top-text">Born in curiosity, raised by dedication.</p>
+            </a>
+          </div>
+          <div id="w-node-_70b3894f-ad3a-f5e5-ddd7-7ba668131849-68131841">
+            <a href="https://preview.webflow.com/preview/bo-template?utm_medium=preview_link&amp;utm_source=designer&amp;utm_content=bo-template&amp;preview=bec49be0575aff15abc5d07969935906&amp;workflow=preview" target="_blank" class="button w-button">view on webflow</a>
+          </div>
+        </div>
+        <div class="margin-50px">
+          <div class="container">
+            <div class="footer-grid">
+              <div>
+                <div>
+                  <p class="subhead">Bo is an award UI/UX designs and branding agency based in New York, USA.<strong><br/></strong></p>
+                </div>
+                <div class="margin-25px">
+                  <div class="social-wrapper-footer _2">
+                    <a href="https://www.instagram.com/" target="_blank" class="social-icon-wrapper w-inline-block"><img src="https://uploads-ssl.webflow.com/623b341060c39e4dc7b99525/623c7e9fd621599df95cd867_instagram.png" loading="lazy" width="20" alt="" class="social-image"/>
+                      <div class="social-overlay"></div>
+                    </a>
+                    <a href="https://www.twitter.com/" target="_blank" class="social-icon-wrapper w-inline-block"><img src="https://uploads-ssl.webflow.com/623b341060c39e4dc7b99525/623c7e9f37b44ce7ee306601_facebook.png" loading="lazy" width="20" alt="" class="social-image"/>
+                      <div class="social-overlay"></div>
+                    </a>
+                    <a href="https://www.linkedin.com/" target="_blank" class="social-icon-wrapper w-inline-block"><img src="https://uploads-ssl.webflow.com/623b341060c39e4dc7b99525/623c7e9f5f08d7d3daed0dd9_linkedin.png" loading="lazy" width="18" alt="" class="social-image"/>
+                      <div class="social-overlay"></div>
+                    </a>
+                    <a href="https://www.dribbble.com/" target="_blank" class="social-icon-wrapper w-inline-block"><img src="https://uploads-ssl.webflow.com/623b341060c39e4dc7b99525/623c7e9f66ae86ca37aaa98c_dribbble.png" loading="lazy" width="20" alt="" class="social-image"/>
+                      <div class="social-overlay"></div>
+                    </a>
+                    <a href="https://www.dribbble.com/" target="_blank" class="social-icon-wrapper w-inline-block"><img src="https://uploads-ssl.webflow.com/623b341060c39e4dc7b99525/623c7e9e6ad17ea295c46e27_twiiter.png" loading="lazy" width="20" alt="" class="social-image"/>
+                      <div class="social-overlay"></div>
+                    </a>
+                  </div>
+                </div>
+              </div>
+              <div class="empty-block"></div>
+              <div>
+                <p class="subhead">New South Head Rd, Double Bay NSW 1004, New York<a href="https://vest-template.webflow.io/#"><strong><br/></strong></a>
+                </p>
+                <div class="margin-25px">
+                  <div id="w-node-_70b3894f-ad3a-f5e5-ddd7-7ba66813186e-68131841">
+                    <a href="#" class="button-with-line _2 email w-inline-block">
+                      <div>
+                        <div>contact@bo.com</div>
+                        <div class="button-line">
+                          <div class="button-line-absolute"></div>
+                        </div>
+                      </div>
+                    </a>
+                    <div class="margin-15px">
+                      <a href="#" class="button-with-line _2 email w-inline-block">
+                        <div>
+                          <div>+1300 877 503</div>
+                          <div class="button-line">
+                            <div class="button-line-absolute"></div>
+                          </div>
+                        </div>
+                      </a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="margin-30px">
+              <div class="copyright-flex">
+                <p class="copyright">© This is a <a href="https://www.denibozo.com/" target="_blank" class="dark-link">Deni Bozo</a> template powered by <a href="https://webflow.com/" target="_blank" class="dark-link">Webflow</a>
+                </p>
+                <div class="copyright-divder"></div>
+                <a href="template-info/style-guide.html" class="dark-link">Style Guide</a>
+                <div class="copyright-divder"></div>
+                <a href="template-info/licensing.html" class="dark-link">Licensing</a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+</>
+  )
+}
+
+export default Footer
---
+++ src/component/Header.js
@@ -0,0 +1,54 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+  <>
+ <div class="hero">
+     
+     <div data-collapse="medium" data-animation="default" data-duration="400" data-w-id="78fd20c4-2009-a6a3-7378-0df69d2dbd82" data-easing="ease" data-easing2="ease" role="banner" class="navbar w-nav">
+       <div class="container-navigation grid w-container">
+         <a href="index.html" id="w-node-_78fd20c4-2009-a6a3-7378-0df69d2dbd84-9d2dbd82" data-w-id="78fd20c4-2009-a6a3-7378-0df69d2dbd84" aria-current="page" class="logo-link w-inline-block w--current"><img src="https://uploads-ssl.webflow.com/623b341060c39e4dc7b99525/623c6048d9f33b37ec6d4826_logo.png" loading="lazy" width="138" alt="" class="bo-logo"/>
+           <p class="top-text">Born in curiosity, raised by dedication.</p>
+         </a>
+         <div id="w-node-_78fd20c4-2009-a6a3-7378-0df69d2dbd88-9d2dbd82" class="navigation-flex">
+           <nav role="navigation" id="w-node-_78fd20c4-2009-a6a3-7378-0df69d2dbd89-9d2dbd82" class="nav-menu w-nav-menu">
+             <a href="index.html" aria-current="page" class="navlink w-inline-block w--current">
+               <div class="text-primary">Home</div>
+             </a>
+             <a href="about.html" class="navlink w-inline-block">
+               <div class="text-primary">About</div>
+             </a>
+             <a href="work.html" class="navlink w-inline-block">
+               <div class="text-primary">Work</div>
+             </a>
+             <a href="news.html" class="navlink w-inline-block">
+               <div class="text-primary">News</div>
+             </a>
+             <a href="contact.html" class="navlink w-inline-block">
+               <div class="text-primary">Contact</div>
+             </a>
+
+             <a href="contact.html" class="navlink w-inline-block">
+               <div data-node-type="commerce-cart-wrapper" data-w-id="78fd20c4-2009-a6a3-7378-0df69d2dbd9d" data-open-product="" data-wf-cart-type="rightSidebar" data-wf-cart-query="" data-wf-page-link-href-prefix="" class="w-commerce-commercecartwrapper cart">
+               <a href="#" data-node-type="commerce-cart-open-link" class="w-commerce-commercecartopenlink cart-button w-inline-block"><img src="https://uploads-ssl.webflow.com/623b341060c39e4dc7b99525/623c61f9f88d13469188a7d8_cart.png" width="19" alt="" class="cart-image"/>
+                  <div class="w-commerce-commercecartopenlinkcount cart-quantity">0</div>
+                </a>
+               </div>
+             </a>
+           </nav>
+       
+
+
+         </div>
+       </div>
+     </div>
+     
+  
+   </div>
+
+
+    </>
+  )
+}
+
+export default Header
