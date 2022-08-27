721aac1dc9f67e33420f039548b70f3f3b78900e
achraf9585 <achraf9585@gmail.com>
2022-08-27 11:10:00 +0100

    contact us added 2

--- src/App.js
+++ src/App.js
@@ -9,7 +9,8 @@ function App() {
     <>
       <Router>
       <Routes>
-      <Route path="/freelance-app" element={<Home/>}  exact />
+      <Route path="/freelance-app/" element={<Home/>}  exact />
+      <Route path="/" element={<Home/>}  exact />
       <Route path="/contact" element={<Contact/>}  exact />
         </Routes>
 
